1778f96872ac17f83b8f581c13af0df4afe464de
Thiago Mendes do Nascimento <124217018+Offiline26@users.noreply.github.com>
2025-02-25 23:09:04 -0300

    Delete 2tdspw-api-rest-master/src/main/java/br/com/fiap/api_rest/Dto directory

--- 2tdspw-api-rest-master/src/main/java/br/com/fiap/api_rest/Dto/LivroRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.api_rest.Dto;
-
-public record LivroRequestDTO(String titulo,
-                              String autor) {
-
-}
--- 2tdspw-api-rest-master/src/main/java/br/com/fiap/api_rest/Dto/LivroResponse.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.api_rest.Dto;
-
-public record LivroResponse(String infoLivro) {
-
-}
